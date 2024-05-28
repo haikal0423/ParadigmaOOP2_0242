85854a3dc978037c52568c27cfc70428ed15b6f5
haikal0423 <mhaikal23azka@gmail.com>
2024-05-28 14:49:46 +0700

    Menambahkan class orang

--- ParadigmaOOP2_0242/ParadigmaOOP2_0242.cpp
+++ ParadigmaOOP2_0242/ParadigmaOOP2_0242.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang dibuat dngan umur" << umur << "\n" << endl;
+	}
+};
+
 
